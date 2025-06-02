46d221eee55644e659b5b87f2b13795d3639e0d1
Schnarriii <aa.schnarr@gmail.com>
2025-06-02 14:03:05 +0200

    fine tuning of wpm
    fine tuned the wpm parameters in the OCRA registers and solved some bugs

--- Robotic_Competition.X/motor_driver.c
+++ Robotic_Competition.X/motor_driver.c
@@ -10,16 +10,19 @@
 //              Break   --> PB1_H, PD7_H
 
 
-void motor_inti(void){
+void motor_init(void){
     TCCR1A = (1<<COM1A1)|(1<<COM1B1)|(1<<WGM10);
     TCCR1B = (1<<WGM12)|(3<<CS10);
-    DDRB |= (1<<LEFT_MOTOR_PWM_PIN), (1<<RIGHT_MOTOR_PWM_PIN);
-    DDRD |= (1<<LEFT_MOTOR_PIN), (1<<RIGHT_MOTOR_PIN);
-    PORTD &=^ (1<<LEFT_MOTOR_PIN), (1<<RIGHT_MOTOR_PIN);
-    PORTB &=^ (1<<LEFT_MOTOR_PWM_PIN), (1<<RIGHT_MOTOR_PWM_PIN);
+    DDRB |= (1<<LEFT_MOTOR_PWM_PIN)|(1<<RIGHT_MOTOR_PWM_PIN);
+    DDRD |= (1<<LEFT_MOTOR_PIN)|(1<<RIGHT_MOTOR_PIN);
+    PORTD &=~ (1<<LEFT_MOTOR_PIN)| (1<<RIGHT_MOTOR_PIN);
+    PORTB &=~(1<<LEFT_MOTOR_PWM_PIN)| (1<<RIGHT_MOTOR_PWM_PIN);
 }
 
-void velocity (uint8_t velo_l, uint8_t velo_r){
+void velocity(uint16_t velo_l, uint16_t velo_r){
+    
+    velo_l *= 6;
+    velo_r *= 6;
     OCR1A = velo_r;
     OCR1B = velo_l;
 }
@@ -28,24 +31,24 @@ void driving_direction(char direc_l, char direc_r){
     
     if(direc_l == 'F'){
         PORTB |= (LEFT_MOTOR_PWM_PIN);
-        PORTD &=^ (LEFT_MOTOR_PIN);
+        PORTD &=~ (LEFT_MOTOR_PIN);
     } else if(direc_l == 'R'){
         PORTB |= (LEFT_MOTOR_PWM_PIN);
         PORTD |= (LEFT_MOTOR_PIN);
     } else if(direc_l == 'S'){ //how do is stop it?
-        PORTB &=^ (LEFT_MOTOR_PWM_PIN);
-        PORTD &=^ (LEFT_MOTOR_PIN);
+        PORTB &=~ (LEFT_MOTOR_PWM_PIN);
+        PORTD &=~ (LEFT_MOTOR_PIN);
     }
     
     
      if(direc_r == 'F'){
         PORTB |= (RIGHT_MOTOR_PWM_PIN);
-        PORTD &=^ (RIGHT_MOTOR_PIN);
+        PORTD &=~ (RIGHT_MOTOR_PIN);
     } else if(direc_r == 'R'){
         PORTB |= (RIGHT_MOTOR_PWM_PIN);
         PORTD |= (RIGHT_MOTOR_PIN);
     } else if(direc_r == 'S'){ //how do is stop it?
-        PORTB &=^ (RIGHT_MOTOR_PWM_PIN);
-        PORTD &=^ (RIGHT_MOTOR_PIN);
+        PORTB &=~(RIGHT_MOTOR_PWM_PIN);
+        PORTD &=~ (RIGHT_MOTOR_PIN);
     }
 }
---
+++ Robotic_Competition.X/motor_driver_1.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   motor_driver_1.h
+ * Author: Aaron Schnarr
+ *
+ * Created on 2. Juni 2025, 13:14
+ */
+
+#ifndef MOTOR_DRIVER_1_H
+#define	MOTOR_DRIVER_1_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOTOR_DRIVER_1_H */
+
